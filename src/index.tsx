cf23a0d59c75bcb627ee0aec1fb1e85b92f51a66
christine-aqui <aqui.christine@gmail.com>
2020-04-17 23:26:46 -0400

    basic card, title and content wrapper component with theme applied

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { unregister } from './core'
+import { Content, Title, Card } from './components'
 import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello world</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">Test</Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
